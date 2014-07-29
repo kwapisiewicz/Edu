1d5a1d226c86c8b5303512d648ce2ea192ffa0b3
kwapisiewicz <wojciech.kwapisiewicz@gmail.com>
2014-07-29 21:38:59 +0200

    Solution + project + shell + bootstrapper + initial main module.

---
+++ LibraryManagment.UI/Bootstrapper.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Practices.Prism.UnityExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using System.Windows;
+using Microsoft.Practices.Prism.Modularity;
+
+namespace LibraryManagment.UI
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override System.Windows.DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            Type type1 = typeof(MainModule);
+            ModuleCatalog.AddModule(new ModuleInfo()
+                {
+                    ModuleName = type1.Name,
+                    ModuleType = type1.AssemblyQualifiedName
+                });
+        }
+    }
+}
